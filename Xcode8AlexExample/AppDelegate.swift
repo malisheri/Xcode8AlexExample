ff63913aeefa77b0bd6e648bb34c569f02dc53bb
Mardon Alisheri <malisheri@mardons-mbp.fios-router.home>
2017-03-12 22:33:35 -0400

    Initial Commit

---
+++ Xcode8AlexExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xcode8AlexExample
+//
+//  Created by Mardon Alisheri on 3/12/17.
+//  Copyright © 2017 Mardon Alisheri. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xcode8AlexExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode8AlexExample
+//
+//  Created by Mardon Alisheri on 3/12/17.
+//  Copyright © 2017 Mardon Alisheri. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
